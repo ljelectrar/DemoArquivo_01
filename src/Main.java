69f2989deb5df039767ebaab20c5aa6e38f530b7
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-11-20 12:36:25 -0300

    Ok

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        File file = new File("C:\\Users\\ljele\\IdeaProjects\\in.txt");
+        Scanner sc = null;
+
+        try {
+            sc = new Scanner(file);
+            while(sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e){
+            System.out.println("ERROR:" + e.getMessage());
+        }
+        finally {
+            if(sc != null){
+                sc.close();
+            }
+        }
+
     }
 }
